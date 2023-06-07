8cdc025e7cbf435401b4f6a2026aedb9b8ac2684
Alf-Anas <43590041+Alf-Anas@users.noreply.github.com>
2023-06-07 19:50:29 +0000

    Deploying to gh-pages from @ Alf-Anas/nextjs-pokedex@fdd0de27e3b2dd4baa83218906b6dc736f9db3ad 🚀

---
+++ _next/static/I7ddyyhs7YRwc7JIpwqnw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/02d657094f7b649a.css","static/chunks/pages/index-9492bcbc2bb33000.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
